c3f9c7bff6abad110ed35201be149e903673b0b9
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2026-02-03 13:43:41 +0530

    update vite config | package.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
-  assetsInclude: ['**/*.jsx'],
   optimizeDeps: {
     include: ['react', 'react-dom']
   },
